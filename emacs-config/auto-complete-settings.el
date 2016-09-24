495b3853214b49b7f07d205a4759d51f684afbd2
Natalie Pendragon <natalie.pendragon@gmail.com>
2016-09-23 22:01:21 -0400

    Fix autocomplete settings

--- emacs-config/auto-complete-settings.el
+++ emacs-config/auto-complete-settings.el
@@ -3,10 +3,11 @@
 (use-package auto-complete
   :ensure t
   :diminish
-  :config
-  ((require 'auto-complete-config)
-   (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-   (ac-config-default)))
+  :init
+  (progn
+    (require 'auto-complete-config)
+    (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+    (ac-config-default)))
 
 (provide 'auto-complete-settings)
 ;;; auto-complete-settings.el ends here
