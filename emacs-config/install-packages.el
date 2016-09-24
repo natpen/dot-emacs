28904f6b932f3c0247b701a683d9485a42e47d38
Natalie Pendragon <natalie.pendragon@gmail.com>
2016-09-24 10:15:03 -0400

    Remove prelude-inspired manager package

--- emacs-config/install-packages.el
+++ emacs-config/install-packages.el
@@ -1,8 +1,5 @@
 ;;; -*- mode: emacs-lisp; -*-
 
-;;; This file is a modified version of:
-;;; prelude-packages.el in https://github.com/bbatsov/prelude
-
 (require 'cl)
 (require 'package)
 
@@ -19,37 +16,5 @@
 
 (require 'use-package)
 
-(defvar known-packages
-  '(linum-relative)
-  "A list of packages to ensure are installed at launch.")
-
-(defun known-packages-installed-p ()
-  "Check if all packages in `prelude-packages' are installed."
-  (every #'package-installed-p known-packages))
-
-(defun require-package (package)
-  "Install PACKAGE unless already installed."
-  (unless (memq package known-packages)
-    (add-to-list 'known-packages package))
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(defun require-packages (packages)
-  "Ensure PACKAGES are installed.
-Missing packages are installed automatically."
-  (mapc #'require-package packages))
-
-(defun install-packages ()
-  "Install all packages listed in `known-packages'."
-  (unless (known-packages-installed-p)
-    ;; check for new packages (package versions)
-    (message "%s" "Emacs is now refreshing its package database...")
-    (package-refresh-contents)
-    (message "%s" " done.")
-    ;; install the missing packages
-    (require-packages known-packages)))
-
-;; run package installation
-(install-packages)
-
-(provide 'known-packages)
+(provide 'install-packages)
+;;; install-packages.el ends here
