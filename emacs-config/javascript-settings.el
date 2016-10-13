5575f5ca3e28ddbc70b46a9f319176e5d222dd35
Natalie Pendragon <natalie.pendragon@newscred.com>
2016-10-13 17:41:37 -0400

    Switch to JS2 mode for javascript

--- emacs-config/javascript-settings.el
+++ emacs-config/javascript-settings.el
@@ -1,17 +1,38 @@
 ;;; -*- mode: emacs-lisp; -*-
 
-(add-hook 'js-mode-hook
-	  (lambda ()
-	    (use-package jsfmt
-	      :ensure t
-	      :init
-	      (add-hook 'before-save-hook 'jsfmt-before-save))
-	    (highlight-indentation-mode)
-	    (setq indent-tabs-mode nil)
-	    (setq tab-width 2)
-	    (setq js-indent-level 2)
-	    (yas-reload-all)
-	    (yas-minor-mode)))
+(use-package jsfmt
+  :ensure t
+  :defer t)
+
+(use-package ac-js2
+  :ensure t
+  :defer t
+  :config
+  (setq ac-js2-evaluate-calls t))
+
+(use-package js2-refactor
+  :ensure t
+  :diminish js2-refactor-mode
+  :defer t)
+
+(use-package js2-mode
+  :ensure t
+  :mode ("\\.js\\'" "\\.json\\'")
+  :init
+  (add-hook 'js2-mode-hook (lambda()
+			     (yas-reload-all)
+			     (yas-minor-mode)
+			     (js2-refactor-mode)
+			     (ac-js2-mode)))
+  :config
+  (progn
+    (setq js2-basic-offset 2)
+    (setq js2-highlight-level 3)
+    (setq js2-highlight-external-variables nil)
+    (setq js2-strict-missing-semi-warning nil)
+    ;; (add-hook 'before-save-hook 'jsfmt-before-save)
+    (highlight-indentation-mode)
+    (rename-modeline "js2-mode" js2-mode "JS2")))
 
 (provide 'javascript-settings)
-;;; javascript-settings.symlink ends here
+;;; javascript-settings.el ends here
