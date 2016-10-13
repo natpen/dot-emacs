0b24a5aedf83be02b6a91793f06cafb50110be29
Natalie Pendragon <natalie.pendragon@gmail.com>
2016-10-12 21:29:54 -0400

    Fix Go indentation

--- emacs-config/golang-settings.el
+++ emacs-config/golang-settings.el
@@ -10,19 +10,25 @@
 (use-package go-mode
   :ensure t
   :init
-  (add-hook 'go-mode-hook (lambda()
-  			    (yas-reload-all)
-  			    (yas-minor-mode)))
+  (progn
+    (defun go-set-tab-width ()
+      "Set the tab width."
+      (setq-local tab-width 2))
+    (add-hook 'go-mode-hook 'go-set-tab-width)
+    (add-hook 'go-mode-hook (lambda()
+			      (yas-reload-all)
+			      (yas-minor-mode))))
   :config
-  (progn (setq tab-width 2)
-	 (go-eldoc-setup)
-	 (add-hook 'before-save-hook 'gofmt-before-save))
-  :bind (:map go-mode-map
-	      ("C-c f" . gofmt)
-	      ("C-c C-k" . godoc-at-point)
-	      ("C-c C-r" . go-remove-unused-imports)
-	      ("C-c C-g" . go-goto-imports)
-	      ("M-." . godef-jump)))
+  (progn
+    (go-eldoc-setup)
+    (add-hook 'before-save-hook 'gofmt-before-save))
+  :bind
+  (:map go-mode-map
+	("C-c f" . gofmt)
+	("C-c C-k" . godoc-at-point)
+	("C-c C-r" . go-remove-unused-imports)
+	("C-c C-g" . go-goto-imports)
+	("M-." . godef-jump)))
 
 (provide 'golang-settings)
 ;;; golang-settings.el ends here
