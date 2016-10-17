e3e6df55d750ea221ea13365ce4459d7ec4e86db
Natalie Pendragon <natalie.pendragon@gmail.com>
2016-10-17 09:30:19 -0400

    Add melpa-stable package repository

--- emacs-config/package-management-init.el
+++ emacs-config/package-management-init.el
@@ -3,9 +3,10 @@
 (require 'cl)
 (require 'package)
 
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "http://melpa.org/packages/")
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (package-initialize)
