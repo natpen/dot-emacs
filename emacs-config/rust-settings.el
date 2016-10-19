d30a79f42340cebbc0c55898db3828b84dccb9d9
Natalie Pendragon <natalie.pendragon@newscred.com>
2016-10-19 18:37:17 -0400

    Improve Rust settings loading behavior

--- emacs-config/rust-settings.el
+++ emacs-config/rust-settings.el
@@ -2,42 +2,42 @@
 
 (use-package cargo
   :ensure t
-  :diminish cargo-minor-mode
-  :mode ("\\.rs\\'" . cargo-minor-mode))
+  :diminish (cargo-minor-mode . "crgo")
+  :defer t)
 
 (use-package company
   :ensure t
-  :diminish "CO"
-  :mode ("\\.rs\\'" . company-mode)
+  :diminish "cmpny"
+  :defer t
   :init (setq annotations-company-tooltip-align t))
 
 (use-package flycheck-rust
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package racer
   :ensure t
-  :diminish racer-mode
-  :mode ("\\.rs\\'" . racer-mode)
-  :init (setq racer-rust-src-path "~/code/rust/src/")
-  :config (progn
-	    (eldoc-mode)
-	    (company-mode)))
+  :diminish (racer-mode . "rcr")
+  :defer t
+  :init (progn
+          (setq racer-rust-src-path "~/code/rust/src/")
+          (add-hook 'racer-mode-hook (lambda()
+                                       (company-mode)
+                                       (eldoc-mode)))))
 
 (use-package rust-mode
   :ensure t
   :init
   (add-hook 'rust-mode-hook (lambda()
-			      (yas-reload-all)
-			      (yas-minor-mode)))
-  :config (progn
-	    (racer-mode)
-	    (highlight-indentation-mode)
-	    (cargo-minor-mode)
-	    (flycheck-rust-setup)
-	    (add-hook 'before-save-hook 'rust-format-buffer))
+        		      (yas-reload-all)
+        		      (yas-minor-mode)
+                              (highlight-indentation-mode)
+                              (cargo-minor-mode)
+                              (flycheck-rust-setup)
+                              (racer-mode)))
+  :config (add-hook 'before-save-hook 'rust-format-buffer)
   :bind (:map rust-mode-map
-	      ("C-c [tab]" . rust-format-buffer)
-	      ("[tab]" . company-indent-or-complete-common)))
+	      ("<tab>" . company-indent-or-complete-common)))
 
 (provide 'rust-settings)
 ;;; rust-settings.symlink ends here
